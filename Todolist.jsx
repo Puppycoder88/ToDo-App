ba9eeb121a45a46b8daae97dc0c8bdf998a2b5b2
Puppycoder88 <jaingarima1795@gmail.com>
2024-07-01 18:42:04 +0530

    Update Todolist.jsx

--- Todolist.jsx
+++ Todolist.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import DeleteIcon from '@mui/icons-material/Delete';
-// import { Edit } from "./Edit";
-// import EditIcon from '@mui/icons-material/Edit';
 
 const Todolist = (props) => {
   return (
@@ -21,59 +19,3 @@ const Todolist = (props) => {
 
 export default Todolist;
 
-// import React, { useState } from "react";
-// import "bootstrap/js/dist/button";
-// import AddIcon from '@mui/icons-material/Add';
-// import DeleteIcon from '@mui/icons-material/Delete';
-// import Button from '@mui/material/Button';
-
-// const Todolist = () => {
-//   const [num, setNum] = useState(0);
-
-//   const incNum = () => {
-//     setNum(num + 1);
-//   };
-
-//   const decNum = () => {
-//     if (num > 0) {
-//       setNum(num - 1);
-//     } else {
-//       alert("Sorry, zero limit reacted");
-//       setNum(0);
-//     }
-//   };
-
-//   return (
-//     <>
-//       <div className="main_div">
-//         <div className="center_div">
-//           <h1>{num}</h1>
-//           <div className="btn_div">
-//             <Button onClick={incNum} className="btn_green">
-//               <AddIcon/>
-//             </Button>
-//             <Button onClick={decNum} className="btn_red">
-//               <DeleteIcon/>
-//             </Button>
-//           </div>
-//         </div>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default Todolist;
-
-// Digital clock//
-// import React from "react";
-// import { Clock } from "digital-clock-react";
-
-// function Todolist() {
-//   return (
-//     <div className="clock_div">
-//       <Clock />
-//     </div>
-//   );
-// }
-
-// export default Todolist;
